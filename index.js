d8e39cd0485f56287ab1fa8047b1eac0979cdb9f
Siyaka Fatimat <siyakafatimat@gmail.com>
2023-10-08 12:10:55 +0100

    first commit

--- index.js
+++ index.js
@@ -2,17 +2,41 @@ const openToggle = document.querySelector(".open-toggle");
 const closeToggle = document.querySelector(".close-toggle");
 const links = document.querySelector(".nav-links");
 
-openToggle.addEventListener('click', function(){
-  console.log(links.classList)
-  console.log(links.classList.contains('show-links'))
 
-  if (links.classList.contains('show-links')){
-    (links.classList.remove('show-links'))
+//toggle navigation
+
+openToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
+// main container [frequently asked questions]
+
+//show content
+function showHiddenContent(event) {
+  const targetButtonId = event.target.getAttribute("data-target");
+  const hiddenContent = document.querySelector(
+    `.toggle-content[data-content-id="${targetButtonId}"]`
+  );
+  if (hiddenContent){
+    hiddenContent.style.display = "block";
   }
-  else{
-    links.classList.add('show-links')
+}
+//hide content
+function hideHiddenContent(event) {
+  const targetButtonId = event.target.getAttribute("data-target");
+  const hiddenContent = document.querySelector(
+    `.toggle-content[data-content-id="${targetButtonId}"]`
+  );
+  if (hiddenContent){
+    hiddenContent.style.display = "none";
   }
-})
-closeToggle.addEventListener('click', function(){
-  links.classList.remove('show-links')
-})
+}
+  const toggleShowIcons = document.querySelectorAll(".show-icon");
+  toggleShowIcons.forEach(button => {
+    button.addEventListener("click", showHiddenContent);
+  })
+  
+  const toggleHideIcon = document.querySelectorAll(".hidden-icon");
+  toggleHideIcon.forEach(button => {
+    button.addEventListener("click", hideHiddenContent);
+  });
--- registerpage.js
+++ registerpage.js
@@ -1,17 +1,19 @@
 const registerbtn = document.querySelector(".register-btn");
-const popUpMessage = document.querySelector(".pop-up-message");
+const popUpMessage = document.querySelector(".success-message-container");
 const closebtn = document.querySelector(".btn");
+// const overlay = document.querySelector(".overlay");
 
 registerbtn.addEventListener("click", function () {
   popUpMessage.style.display = "block";
-  console.log(popUpMessage);
-  overlay.style.display = "block"
+  // console.log(popUpMessage);
+  document.querySelector(".overlay").style.display = "block";
+  // overlay.style.display = "block";
   document.body.style.overflow = "hidden";
-  
 });
 function closewindow() {
   popUpMessage.style.display = "none";
+  document.querySelector(".overlay").style.display = "none";
   document.body.style.overflowx = "auto";
 }
 closebtn.addEventListener("click", closewindow);
-// console.log(closebtn);
+
