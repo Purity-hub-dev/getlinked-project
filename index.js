3d04d33a5930a382a9bebe6b4d62f463cead2413
Siyaka Fatimat <siyakafatimat@gmail.com>
2023-10-09 16:14:58 +0100

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const openToggle = document.querySelector(".open-toggle");
 const closeToggle = document.querySelector(".close-toggle");
 const links = document.querySelector(".nav-links");
 
-
 //toggle navigation
 
 openToggle.addEventListener("click", function () {
@@ -12,31 +11,55 @@ openToggle.addEventListener("click", function () {
 // main container [frequently asked questions]
 
 //show content
-function showHiddenContent(event) {
-  const targetButtonId = event.target.getAttribute("data-target");
-  const hiddenContent = document.querySelector(
-    `.toggle-content[data-content-id="${targetButtonId}"]`
-  );
-  if (hiddenContent){
-    hiddenContent.style.display = "block";
-  }
-}
-//hide content
-function hideHiddenContent(event) {
-  const targetButtonId = event.target.getAttribute("data-target");
-  const hiddenContent = document.querySelector(
-    `.toggle-content[data-content-id="${targetButtonId}"]`
-  );
-  if (hiddenContent){
-    hiddenContent.style.display = "none";
-  }
-}
-  const toggleShowIcons = document.querySelectorAll(".show-icon");
-  toggleShowIcons.forEach(button => {
-    button.addEventListener("click", showHiddenContent);
+// function showHiddenContent(event) {
+//   const targetButtonId = event.target.getAttribute("data-target");
+//   const hiddenContent = document.querySelector(
+//     `.toggle-content[data-content-id="${targetButtonId}"]`
+//   );
+//   if (hiddenContent){
+//     hiddenContent.style.display = "block";
+//   }
+// }
+// //hide content
+// function hideHiddenContent(event) {
+//   const targetButtonId = event.target.getAttribute("data-target");
+//   const hiddenContent = document.querySelector(
+//     `.toggle-content[data-content-id="${targetButtonId}"]`
+//   );
+//   if (hiddenContent){
+//     hiddenContent.style.display = "none";
+//   }
+// }
+//   const toggleShowIcons = document.querySelectorAll(".show-icon");
+//   toggleShowIcons.forEach(button => {
+//     button.addEventListener("click", showHiddenContent);
+//   })
+
+//   const toggleHideIcon = document.querySelectorAll(".hidden-icon");
+//   toggleHideIcon.forEach(button => {
+//     button.addEventListener("click", hideHiddenContent);
+//   });
+
+// other way to do it
+// const buttons = document.querySelectorAll(".toggle-container");
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (event) {
+//     // console.log(event.currentTarget.parentElement.parentElement)
+//     const question = event.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-content")
+//   });
+// });
+
+//by selector
+const questions = document.querySelectorAll(".main-article")
+questions.forEach(function(question){
+  button = question.querySelector(".toggle-container")
+  button.addEventListener("click", function(){
+    questions.forEach(function(icon){
+      if(icon !== question){
+        icon.classList.remove('show-content')
+      }
+    })
+    question.classList.toggle("show-content")
   })
-  
-  const toggleHideIcon = document.querySelectorAll(".hidden-icon");
-  toggleHideIcon.forEach(button => {
-    button.addEventListener("click", hideHiddenContent);
-  });
+});
