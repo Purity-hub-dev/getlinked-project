912a298a92ab3753f3a85a035b033580bba096ff
Siyaka Fatimat <siyakafatimat@gmail.com>
2023-11-10 12:26:31 +0100

    first commit

--- index.js
+++ index.js
@@ -54,15 +54,93 @@ closeToggle.addEventListener("click", function () {
 // });
 
 //by selector
-const questions = document.querySelectorAll(".main-article")
-questions.forEach(function(question){
-  button = question.querySelector(".toggle-container")
-  button.addEventListener("click", function(){
-    questions.forEach(function(icon){
-      if(icon !== question){
-        icon.classList.remove('show-content')
+const questions = document.querySelectorAll(".main-article");
+questions.forEach(function (question) {
+  button = question.querySelector(".toggle-container");
+  button.addEventListener("click", function () {
+    questions.forEach(function (icon) {
+      if (icon !== question) {
+        icon.classList.remove("show-content");
       }
-    })
-    question.classList.toggle("show-content")
-  })
+    });
+    question.classList.toggle("show-content");
+  });
 });
+
+//counter
+
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const deadlineContainer = document.querySelector(".header-counter");
+const dealineFormat = document.querySelectorAll(".header-counter h4");
+
+const futureDate = new Date(2024, 0, 8, 10, 30, 0);
+
+const futureYear = futureDate.getFullYear();
+const futureMonth = futureDate.getMonth();
+const futureDay = futureDate.getDay();
+const futureMinutes = futureDate.getMinutes();
+const futureSeconds = futureDate.getSeconds();
+// console.log(futureYear);
+
+// how many minutes make a day
+
+//1s = 1000ms
+//1m = 60s
+//1hr = 60m
+//1d = 24hr
+const futureTime = futureDate.getTime();
+// console.log(futureTime);
+function getFutureTime() {
+  const currentTime = new Date().getTime();
+  const getRemainingTime = futureTime - currentTime;
+  // console.log(getRemainingTime);
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  const days = Math.floor(getRemainingTime / oneDay);
+  const hours = Math.floor((getRemainingTime % oneDay) / oneHour);
+  const minutes = Math.floor((getRemainingTime % oneHour) / oneMinute);
+  const seconds = Math.floor((getRemainingTime % oneMinute) / 1000);
+  // console.log(seconds);
+
+  const values = [days, hours, minutes, seconds];
+  function addZero(item) {
+    if (item < 10) {
+      return `0${item}`;
+    }
+    return item;
+  }
+  dealineFormat.forEach(function (item, index) {
+    item.innerHTML = addZero(values[index]);
+  });
+  if (getRemainingTime < 0) {
+    deadlineContainer.innerHTML = `<h4>Sorry, Registration Is Closed</h4>`;
+  }
+}
+setInterval(getFutureTime, 1000);
+getFutureTime();
